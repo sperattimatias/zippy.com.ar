ae46fc0a2a4eab7d646bd1347fac52c888af7825
Matias <sperattirmatias@gmail.com>
2026-03-03 21:08:40 -0300

    Fix CI workflow and test/typecheck failures

--- services/auth/jest.config.js
+++ services/auth/jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
     '^.+\\.(t|j)s$': 'ts-jest',
   },
   testRegex: '.*\\.spec\\.ts$',
+  moduleNameMapper: {
+    '^@shared/(.*)$': '<rootDir>/../../shared/$1',
+  },
   moduleFileExtensions: ['ts', 'js', 'json'],
 };
